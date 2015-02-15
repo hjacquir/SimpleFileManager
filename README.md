a05ad6adc6da66268e22772dbcf90e748f09b4eb
hjacquir <jacquirhatim@gmail.com>
2015-02-15 18:43:12 +0100

    add DatabaseManager with mapping files

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ A simple file manager
 WIP :
 =====
 - Functional Analysis : [see here !] (doc/Analyse.md)
+
+REQUIREMENTS :
+===============
+
+- Doctrine using APC Cache for PHP :
+
+        sudo apt-get install php-apc && sudo service apache2 restart
---
+++ src/Hj/DatabaseManager.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Created by Hatim Jacquir
+ * @author : Hatim Jacquir <jacquirhatim@gmail.com>
+ */
+
+namespace Hj;
+
+use Doctrine\Common\Cache\ApcCache;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\XmlDriver;
+
+/**
+ * I manage the database by using Doctrine Entity Manager
+ * I use APCCache with Doctrine
+ * I use Xml Mapping File
+ *
+ * Class DatabaseManager
+ * @package Hj
+ */
+class DatabaseManager
+{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * @param ApcCache $cache
+     * @param Configuration $configuration
+     * @param array $connexion
+     */
+    public function __construct(ApcCache $cache, Configuration $configuration, array $connexion)
+    {
+        $entityMappingPath = __DIR__ . '/Mapping/';
+        $proxyDir = __DIR__ . '/doctrineProxies/';
+
+        $driver = new XmlDriver(array($entityMappingPath));
+
+        $configuration->setProxyDir($proxyDir);
+        $configuration->setProxyNamespace('DoctrineProxy');
+        $configuration->setMetadataDriverImpl($driver);
+        $configuration->setMetadataCacheImpl($cache);
+        $configuration->setQueryCacheImpl($cache);
+
+        $this->entityManager = EntityManager::create($connexion, $configuration);
+    }
+
+    /**
+     * @return \Doctrine\ORM\EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->entityManager;
+    }
+}
